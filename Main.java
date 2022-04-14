275f93bcb2094673d7bee4f722f00c141c5a8099
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 14:47:10 -0500

    test cart class setup

--- Main.java
+++ Main.java
@@ -4,9 +4,18 @@ import models.Store;
 
 public class Main{
     public static void main(String[] args) {
-        Item itemOne = new Item("Celery", 0);
+        Item itemOne = new Item("Celery", 1);
         Item itemTwo = new Item(itemOne);
         itemTwo.setName("Spinach");
         System.out.println(itemOne + "\n" + itemTwo);
+
+        Cart cart = new Cart();
+        cart.add(new Item("Celery", 0.99));
+        cart.add(new Item("Celery", 0.99));
+        cart.add(new Item("Spinach", 0.99));
+        cart.add(new Item("Coriander", 1.29));
+
+        System.out.println(cart);
+
     }
 }
