b7b50e2595feb111300f2f180a150c2fa731eedd
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 14:07:16 -0500

    protect Item from unchacked exceptions

--- models/Item.java
+++ models/Item.java
@@ -5,6 +5,12 @@ public class Item{
     private double price;
 
     public Item(String name, double price){
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        if(price < 0){
+            throw new IllegalArgumentException("Price cannot be less than zero.");
+        }
         this.name = name;
         this.price = price;
     }
@@ -23,10 +29,16 @@ public class Item{
     }
 
     public void setName(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
         this.name = name;
     }
 
     public void setPrice(double price) {
+        if(price < 0){
+            throw new IllegalArgumentException("Price cannot be less than zero.");
+        }
         this.price = price;
     }
 
