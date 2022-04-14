61b77d7a3dc62d09f7dfb3b5ac7e8e3065c7f72f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 13:56:58 -0500

    fields, setters, getters, constractors done

--- models/Item.java
+++ models/Item.java
@@ -1,5 +1,37 @@
 package models;
 
 public class Item{
-    
+    private String name;
+    private double price;
+
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(Item source){
+        this.name = source.name;
+        this.price = source.price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString(){
+        return (this.name + ": $" + this.price + " ");
+    }
+
 }
