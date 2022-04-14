d3bf8b7fec143cc9a714e12aa6747e06d17269fe
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 14:00:38 -0500

    check if constractors,getters and setters working properly

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ import models.Store;
 
 public class Main{
     public static void main(String[] args) {
-        
+        Item itemOne = new Item("Celery", 0.99);
+        Item itemTwo = new Item(itemOne);
+        itemTwo.setName("Spinach");
+        System.out.println(itemOne + "\n" + itemTwo);
     }
 }
