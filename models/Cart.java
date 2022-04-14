4f20bd8116991ae3a87d4c76c8314a14de574733
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 14:44:06 -0500

    basic cart setup

--- models/Cart.java
+++ models/Cart.java
@@ -1,7 +1,60 @@
 package models;
 
-public class Cart{
+import java.util.ArrayList;
 
+public class Cart{
+    ArrayList<Item> items;
+
+    public Cart(){
+        items = new ArrayList<Item>();
+    }
+
+    public Item getItem(int index){
+        return new Item(items.get(index));
+    }
+
+    public void setItem(int index, Item item){
+        items.set(index, new Item(item));
+    }
+
+    public boolean add(Item item){
+        if(items.contains(item)){
+            return false;
+        }
+        items.add(new Item(item));
+        return true;
+    }
+
+    public void remove(String name){
+        for (int i = 0; i < items.size(); i++) {
+            if(items.get(i).getName().equals(name)){
+                items.remove(i);
+            }
+        }
+    }
+
+    public String checkout(){
+        double subtotal = 0;
+        for (int i = 0; i < items.size(); i++) {
+            subtotal += items.get(i).getPrice();
+        }
+        double tax = subtotal * .13;
+        double total = tax + subtotal;
+
+        return "\tRECEIPT\n\n" +
+        "\tSubtotal: $" + subtotal + "\n" +
+        "\tTax: $" + tax + "\n" +
+        "\tTotal: $" + total + "\n";
+    }
+
+    public String toString(){
+        String temp = "";
+        for (int i = 0; i < items.size(); i++) {
+            temp += items.get(i).toString();
+            temp += "\n";
+        }
+        return temp;
+    }
 
 
 }
